c41605199ff30ce7a677f50c001388bb1b336dd1
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 14:42:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,16 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  fruit = ["apple", "banana", "orange", "kiwi"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  fruits = ["strawberry", "watermelon", "lychee", "mango"]
+    fruits.push ("grapes")
+      puts fruits.inspect
+      # > ["strawberry", "watermelon", "lychee", "mango", "grapes"]
 end
 
 def add_element_to_start_of_array(array, element)
